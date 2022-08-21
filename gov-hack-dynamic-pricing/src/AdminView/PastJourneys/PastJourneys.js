f9f78fc26a584e830472fcf303b305e117163cc5
kunalc156 <kunal.choudhary156@gmail.com>
2022-08-21 13:44:44 +1000

    heatmap

--- gov-hack-dynamic-pricing/src/AdminView/PastJourneys/PastJourneys.js
+++ gov-hack-dynamic-pricing/src/AdminView/PastJourneys/PastJourneys.js
@@ -1,12 +1,10 @@
 import lineGraph from "./lineGraph.png";
+import heatMap from "./heatMap.png";
 import "./PastJourneys.css";
 import { Layout, Menu } from "antd";
 import { Select } from "antd";
-import React from "react";
+import React, { useState } from "react";
 const { Option } = Select;
-const handleChange = (value) => {
-  console.log(`selected ${value}`);
-};
 const { Header, Content } = Layout;
 const items = [
   { label: "Home", key: "home" }, // remember to pass the key prop
@@ -24,8 +22,12 @@ const onClickHandler = (e) => {
     window.location.href = "/Manage";
   }
 };
-
 function PastJourneys() {
+  const [selected, setSelected] = useState("a");
+  const handleChange = (value) => {
+    console.log(`selected ${value}`);
+    setSelected(value);
+  };
   return (
     <Layout>
       <Header className="header">
@@ -61,10 +63,24 @@ function PastJourneys() {
               <Option value="c">Real-Time Capacity</Option>
             </Select>
           </div>
-          <h1 style={{ marginTop: "2em" }}> Displaying - Past Journeys </h1>
-          <div className="PastJourneys">
-            <img src={lineGraph} alt="lineGraph"></img>
-          </div>
+          {selected === "a" ? (
+            <>
+              <h1 style={{ marginTop: "2em" }}> Displaying - Past Journeys </h1>
+              <div className="PastJourneys">
+                <img src={lineGraph} alt="lineGraph"></img>
+              </div>
+            </>
+          ) : (
+            <>
+              <h1 style={{ marginTop: "2em" }}>
+                {" "}
+                Displaying - Occupancy Heat Map{" "}
+              </h1>
+              <div className="PastJourneys">
+                <img src={heatMap} alt="heatMap"></img>
+              </div>
+            </>
+          )}
         </Content>
       </Layout>
     </Layout>
